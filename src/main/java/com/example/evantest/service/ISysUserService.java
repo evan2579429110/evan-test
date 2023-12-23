9f4379d2f09459d12ed902d0ee0d9a047d8bbb46
yuzhiwen <yuzhiwen@1365325449937130.onaliyun.com>
2023-12-23 16:41:25 +0800

    增加一些工具类

---
+++ src/main/java/com/example/evantest/service/ISysUserService.java
@@ -0,0 +1,16 @@
+package com.example.evantest.service;
+
+import com.example.evantest.entity.SysUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户信息表 服务类
+ * </p>
+ *
+ * @author evan
+ * @since 2023-12-23
+ */
+public interface ISysUserService extends IService<SysUser> {
+
+}
